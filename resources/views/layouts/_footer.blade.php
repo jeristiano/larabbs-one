2d3b3adb5e04a6822b5fe7358c474ddec2c67e1c
jeristiano <jeremykuang@foxmail.com>
2018-04-07 13:49:03 +0000

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+<div class="container">
+    <p class="pull-left">
+         由<a href="http://www.jeristiano.win" target="_blank">禅匡(jeristiano)</a>开发  <span style="color: #e27575;font-size: 14px;
+">❤</span>
+    </p>
+
+    <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+</div>
+</footer>
