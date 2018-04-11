1548d6910feae6f5dbce61bd476ff933d33c6a67
jeristiano <jeremykuang@foxmail.com>
2018-04-11 09:38:55 +0000

    权限控制

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+
+    }
+
     //个人中心展示页
     public function show(User $user)
     {
@@ -19,12 +25,14 @@ class UserController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //更新资料
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id,362);
