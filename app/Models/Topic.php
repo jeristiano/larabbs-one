6588d29b2c6b512277ec9342e29c2d4947db4ab3
jeristiano <jeremykuang@foxmail.com>
2018-04-12 12:17:01 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -7,14 +7,46 @@ class Topic extends Model
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 
     //一个话题隶属于一个分类
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
+
     //一个话题属于一个作者
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query = $this->recent();
+                break;
+
+            default:
+                $query = $this->recentReplied();
+                break;
+        }
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
+
 
 }
 
