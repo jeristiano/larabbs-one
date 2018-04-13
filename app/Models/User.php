6b0307cabcabf173df6540d6522d9cd84e5eebdc
jeristiano <jeremykuang@foxmail.com>
2018-04-13 08:32:07 +0000

    author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,11 @@ class User extends Authenticatable
     public function topics(){
         return $this->hasMany(Topic::class);
     }
+
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+
 }
