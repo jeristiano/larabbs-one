8828fee86b4cb28cbacd7b264f2f219623d12b74
jeristiano <jeremykuang@foxmail.com>
2018-04-14 14:14:23 +0000

    user can create reply

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -4,38 +4,21 @@ namespace App\Http\Requests;
 
 class ReplyRequest extends Request
 {
-    public function rules()
+
+        public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
+        return [
+            'contents' => 'required|min:2',
+        ];
     }
+
+
 
     public function messages()
     {
         return [
             // Validation messages
+            'contents.required'=>'最少输入两字符'
         ];
     }
 }
