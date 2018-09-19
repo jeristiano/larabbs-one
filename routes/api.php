79a318322c7ce2645039e3fb4d34167830a84453
jeristiano <jeremykuang@foxmail.com>
2018-09-19 13:45:16 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 $api = app('Dingo\Api\Routing\Router');
-$api->version('v1', [
+$api->version('v1',
+    [
     'namespace' => 'App\Http\Controllers\Api'
-], function ($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    //用户注册接口
-    $api->post('users','UsersController@store')
-        ->name('api.users.store');
+
+],
+    function ($api) {
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+
+    ], function ($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
